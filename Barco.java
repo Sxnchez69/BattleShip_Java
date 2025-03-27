2f0a1d5c3a52cd8760709f52100e673add4c8133
Sxnchez <raulsanpacar@gmail.com>
2025-03-27 13:45:26 +0100

    Cambios en tamaño y estado_barco

--- Barco.java
+++ Barco.java
@@ -1,27 +1,23 @@
 public class Barco {
     protected String tipo;
-    protected int[] tamaño;
+    protected int tamaño;
     protected String orientacion;
     protected int[][] posicion_barco;
-    protected boolean[][] estado_barco;
+    protected boolean[] estado_barco;
 
-    public Barco(int[] tamaño, String orientacion, int fila, int columna) {
+    public Barco(int tamaño, String orientacion, int fila, int columna) {
         this.orientacion = orientacion;
         this.tipo = "Barco";
         this.tamaño = tamaño;
         this.posicion_barco = new int[fila][columna];
-        for (int i = 0; i < posicion_barco.length; i++) {
-            for (int j = 0; j < posicion_barco[i].length; j++) {
-                this.estado_barco[i][j] = false;
-            }
-        }
+        this.estado_barco = new boolean[tamaño];
     }
 
-    public int[] getTamaño() {
+    public int getTamaño() {
         return tamaño;
     }
 
-    public void setTamaño(int[] tamaño) {
+    public void setTamaño(int tamaño) {
         this.tamaño = tamaño;
     }
 
@@ -41,11 +37,11 @@ public class Barco {
         this.posicion_barco = posicion_barco;
     }
 
-    public boolean[][] getEstado_barco() {
+    public boolean[] getEstado_barco() {
         return estado_barco;
     }
 
-    public void setEstado_barco(boolean[][] estado_barco) {
+    public void setEstado_barco(boolean[] estado_barco) {
         this.estado_barco = estado_barco;
     }
 
